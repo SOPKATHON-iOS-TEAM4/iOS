7c96124b6d21579ea0180c9c1a3fb494e7885de1
mcrkgus <mcrkgus@naver.com>
2024-05-16 00:06:10 +0900

    [Setting] Storyboard 삭제

---
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopkathon_iOS_Team4
+//
+//  Created by Gahyun Kim on 2024/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
