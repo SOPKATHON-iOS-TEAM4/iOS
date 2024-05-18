b95b7adf2b850badafb46ac94e11d17a379bdf8e
kingjeongkong <wjdqls9223@gmail.com>
2024-05-19 02:05:30 +0900

    [Chore] #16 - 파일 생성 및 폴더링

---
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/AddTerm/ViewController/AddTermViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddTermViewController.swift
+//  Sopkathon_iOS_Team4
+//
+//  Created by 왕정빈 on 5/19/24.
+//
+
+import UIKit
+
+final class AddTermViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
