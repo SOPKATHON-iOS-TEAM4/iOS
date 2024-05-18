2575518cfb88cb95ea742e72738c080ebef10b6d
kingjeongkong <wjdqls9223@gmail.com>
2024-05-19 02:11:44 +0900

    [Feat] #16 - 단어 Label 구현

--- Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/AddTerm/View/AddTermView.swift
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/AddTerm/View/AddTermView.swift
@@ -7,14 +7,48 @@
 
 import UIKit
 
+import Then
+import SnapKit
+
 final class AddTermView: UIView {
+    
+    // MARK: - UI Properties
+    
+    private let termLabel = UILabel()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI Setting
+    
+    private func setUI() {
+        termLabel.do {
+            $0.text = "단어"
+            $0.font = .pretendardSemiBold(size: 16)
+            $0.textColor = .white0
+        }
+    }
+    
+    private func setHierarchy() {
+       addSubviews(termLabel)
+    }
+    
+    private func setLayout() {
+        termLabel.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).offset(87)
+            $0.leading.equalToSuperview().offset(24)
+            $0.width.equalTo(28)
+        }
+    }
 }
--- Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/AddTerm/ViewController/AddTermViewController.swift
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/AddTerm/ViewController/AddTermViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 final class AddTermViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    private let rootView = AddTermView()
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view = rootView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
