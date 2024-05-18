6c546cb06072f9624b4fe56813cdb2fc43c2c386
FpRaArNkK <mspark1184@naver.com>
2024-05-19 01:57:20 +0900

    [Feat] #12 - TermListDetailView UI 구현

--- Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/TermListDetail/Views/TermListDetailView.swift
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/TermListDetail/Views/TermListDetailView.swift
@@ -13,9 +13,27 @@ import Then
 final class TermListDetailView: UIView {
     
     // MARK: - Properties
+    private var termList: [TermModel] = []
     
     // MARK: - UI Components
     
+    private let cvBackgroundView = UIView()
+    
+    private let termMainCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        
+        $0.itemSize = CGSize(width: 70, height: 30)
+        
+        $0.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        
+        $0.minimumInteritemSpacing = 10
+        $0.minimumLineSpacing = 10
+        $0.scrollDirection = .vertical
+    })
+    
+    private let countLabelBackgroundView = UIView()
+    
+    private let termListCount = UILabel()
     
     // MARK: - Life Cycles
     
@@ -38,14 +56,55 @@ final class TermListDetailView: UIView {
 
 private extension TermListDetailView {
     func setupStyle() {
+        cvBackgroundView.do {
+            $0.backgroundColor = .grayscale70
+            $0.makeRounded(radius: 20)
+        }
         
+        countLabelBackgroundView.do {
+            $0.backgroundColor = .grayscale70
+            $0.makeRounded(radius: 20)
+        }
+        
+        termListCount.do {
+            $0.font = .pretendardMedium(size: 16)
+            $0.textColor = .grayscale50
+        }
     }
     
     func setupHierarchy() {
-
+        [
+            cvBackgroundView,
+            termMainCollectionView,
+            countLabelBackgroundView,
+            termListCount
+        ].forEach { self.addSubviews($0) }
     }
     
     func setupLayout() {
-
+        cvBackgroundView.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(60)
+            $0.trailing.equalToSuperview().offset(-60)
+            $0.top.equalTo(self.safeAreaLayoutGuide).offset(130)
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).offset(-96)
+        }
+        
+        termMainCollectionView.snp.makeConstraints {
+            $0.edges.equalTo(cvBackgroundView)
+        }
+        
+        termListCount.snp.makeConstraints {
+            $0.top.equalTo(cvBackgroundView).offset(35)
+            $0.centerX.equalToSuperview()
+        }
+        
+        countLabelBackgroundView.snp.makeConstraints {
+            $0.center.equalTo(countLabelBackgroundView)
+            $0.leading.equalTo(termListCount).inset(15)
+            $0.trailing.equalTo(termListCount).inset(-15)
+            $0.top.equalTo(termListCount).inset(9)
+            $0.bottom.equalTo(termListCount).inset(-9)
+        }
+        
     }
 }
