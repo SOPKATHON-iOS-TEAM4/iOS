9e8dd72d4f4f0ccf12f703ad7ca73d85fbdd1c7a
kingjeongkong <wjdqls9223@gmail.com>
2024-05-19 01:01:25 +0900

    [Chore] #9 - CreateTermList View 파일 생성 및 폴더링

--- Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Application/SceneDelegate.swift
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Application/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: ViewController())
+        let navigationController = UINavigationController(rootViewController: CreateTermListViewController())
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
---
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/CreateTermList/View/CreateTermListView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateTermListView.swift
+//  Sopkathon_iOS_Team4
+//
+//  Created by 왕정빈 on 5/19/24.
+//
+
+import UIKit
+
+class CreateTermListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
