e3868b22f876aab3f15ad126be66e2eef36555ce
kingjeongkong <wjdqls9223@gmail.com>
2024-05-19 05:21:08 +0900

    [Feat] #21 - 단어장 View Interaction 구현

--- Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Application/SceneDelegate.swift
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Application/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: AddTermViewController())
+        let navigationController = UINavigationController(rootViewController: CreateTermListViewController())
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
--- Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/CreateTermList/ViewController/CreateTermListViewController.swift
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/CreateTermList/ViewController/CreateTermListViewController.swift
@@ -7,20 +7,55 @@
 
 import UIKit
 
+import Then
+
 final class CreateTermListViewController: UIViewController {
     
     // MARK: - Properties
     
     private let rootView = CreateTermListView()
+    
+    private var termList = ""
 
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setTextField()
     }
     
     override func loadView() {
         view = rootView
     }
     
+    // MARK: - TextField  Setting
+    
+    private func setTextField() {
+        rootView.termListTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        termList = textField.text ?? ""
+        rootView.numOfTermListLabel.text = "\(termList.count)/20"
+        setButtonAttributes()
+    }
+    
+    private func isValid(englishTerm: String) -> Bool {
+        let regex = "^.{1,20}$"
+        let termTest = NSPredicate(format: "SELF MATCHES %@", regex)
+        return termTest.evaluate(with: englishTerm)
+    }
+    
+    // MARK: - Button Setting
+    
+    private func setButtonAttributes() {
+        if isValid(englishTerm: termList) {
+            rootView.createButton.setBackgroundColor(.white0, for: .normal)
+            rootView.createButton.isEnabled = true
+        } else {
+            rootView.createButton.setBackgroundColor(.grayscale50, for: .normal)
+            rootView.createButton.isEnabled = false
+        }
+    }
 }
