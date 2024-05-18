c7f49f7c38143064d92209b220634316312b3a1d
kingjeongkong <wjdqls9223@gmail.com>
2024-05-19 02:26:44 +0900

    [Feat] #16 - 영단어 입력 TextField UI 구현

--- Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/AddTerm/View/AddTermView.swift
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/AddTerm/View/AddTermView.swift
@@ -15,6 +15,10 @@ final class AddTermView: UIView {
     // MARK: - UI Properties
     
     private let termLabel = UILabel()
+    
+    private let termTextField = UITextField()
+    
+    private lazy var addTermButton = UIButton()
 
     // MARK: - Initializer
     
@@ -38,10 +42,29 @@ final class AddTermView: UIView {
             $0.font = .pretendardSemiBold(size: 16)
             $0.textColor = .white0
         }
+        
+        termTextField.do {
+            $0.backgroundColor = .grayscale70
+            $0.addPadding(left: 16, right: 16)
+            $0.changePlaceholderColor(forPlaceHolder: "단어를 입력해주세요", forColor: .grayscale50)
+            $0.makeRounded(radius: 15)
+        }
+        
+        addTermButton.do {
+            let attrStr = NSAttributedString(
+                            string: "추가하기",
+                            attributes: [
+                                .font : UIFont.pretendardSemiBold(size: 14),
+                                .foregroundColor : UIColor.white0
+                            ])
+            $0.setAttributedTitle(attrStr, for: .normal)
+            $0.setBackgroundColor(.main60, for: .normal)
+            $0.layer.cornerRadius = 10
+        }
     }
     
     private func setHierarchy() {
-       addSubviews(termLabel)
+       addSubviews(termLabel, termTextField, addTermButton)
     }
     
     private func setLayout() {
@@ -49,6 +72,19 @@ final class AddTermView: UIView {
             $0.top.equalTo(safeAreaLayoutGuide).offset(87)
             $0.leading.equalToSuperview().offset(24)
             $0.width.equalTo(28)
+        }
+        
+        termTextField.snp.makeConstraints {
+            $0.top.equalTo(termLabel.snp.bottom).offset(33)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.height.equalTo(46)
+        }
+        
+        addTermButton.snp.makeConstraints {
+            $0.trailing.equalTo(termTextField).offset(-6)
+            $0.centerY.equalTo(termTextField)
+            $0.width.equalTo(70)
+            $0.height.equalTo(34)
         }
     }
 }
