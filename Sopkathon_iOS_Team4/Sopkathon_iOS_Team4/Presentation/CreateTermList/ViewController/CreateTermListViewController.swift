35d4ca58ef0e55924aac4fcbe1874bc624bd258b
kingjeongkong <wjdqls9223@gmail.com>
2024-05-19 01:36:48 +0900

    [Feat] #9 - 단어장 추가 View UI 구현

--- Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/CreateTermList/ViewController/CreateTermListViewController.swift
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Presentation/CreateTermList/ViewController/CreateTermListViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class CreateTermListViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let rootView = CreateTermListView()
 
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func loadView() {
+        view = rootView
     }
-    */
-
+    
 }
