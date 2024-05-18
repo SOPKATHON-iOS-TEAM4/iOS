126fdb0bd8532af406d0c699f2aeb4adfc117c8d
luckyyy <oyslucy@naver.com>
2024-05-19 05:22:31 +0900

    delete: dto(#19)

--- Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Network/TermHistory/DTO/GetHistoryResponseDTO.swift
+++ Sopkathon_iOS_Team4/Sopkathon_iOS_Team4/Network/TermHistory/DTO/GetHistoryResponseDTO.swift
@@ -7,28 +7,4 @@
 
 import Foundation
 
-struct GetHistoryResponseDTO: Codable {
-    let code: Int
-    let success: Bool
-    let message: String
-    let memorizedWordList: [MemorizedWord]
 
-    private enum CodingKeys: String, CodingKey {
-        case code, success, message
-        case memorizedWordList = "memorized_word_list"
-    }
-}
-
-struct MemorizedWord: Codable {
-    let memorizedWord: WordObject
-
-    private enum CodingKeys: String, CodingKey {
-        case memorizedWord = "memorized_word"
-    }
-}
-
-struct WordObject: Codable {
-    let name: String
-    let type: String
-    let description: String
-}
